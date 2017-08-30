115279844068f6b7ddbb89b7e4f74439c26634e0
Paqwtv <paqwtv@gmail.com>
2017-08-30 16:37:37 +0300

    Add logger.rb and logger_init.rb

--- lib/oxy_logger.rb
+++ lib/oxy_logger.rb
@@ -1,5 +1,83 @@
 require "oxy_logger/version"
 
 module OxyLogger
-  # Your code goes here...
+  	@@files_path
+	# @param value [String] - путь куда сохранять логи
+	def self.files_path= value
+		@@files_path = value
+	end
+
+	# @param bool [Boolean] - сохранение логов локально в файл
+		@@save_to_file
+	def self.save_to_file= bool
+		@@save_to_file = bool
+	end
+
+		@@save_to_db
+	# @param bool [Boolean] - сохранение логов в базу данных
+	def self.save_to_db= bool
+		@@save_to_db = bool
+	end
+
+		@@incoming_params
+	# @param bool [Bollean] - входящие параметры
+	def self.incoming_params= bool
+		@@incoming_params = bool
+	end
+
+		@@output_params
+	# @param bool [Bollean] - исходящие параметры
+	def self.output_params= bool
+		@@output_params = bool
+	end
+
+		@@processing_time
+	# @param bool [Bollean] - время обработки запроса
+	def self.processing_time= bool
+		@@processing_time = bool
+	end
+
+		@@current_user
+	# @param bool [Bollean] - текущий пользователь
+	def self.current_user= bool
+		@@current_user = bool
+	end
+
+	# @param bool [Bollean] - дата и время
+		@@date_time
+	def self.date_time= bool
+		@@date_time = bool
+	end
+
+	# @param bool [Bollean] - вызываемый метод (экшн)
+	 	@@called_method
+	def self.called_method= bool
+		@@called_method = bool
+	end
+
+	# @param bool [Bollean] - вызываемый класс
+	 	@@class_name
+ 	def self.class_name= bool
+ 		@@class_name = bool
+ 	end
+
+	# Конфигурирует сам себя
+	def self.configure
+		yield self		
+	end
+
+	def self.config_oxy_hash
+		{
+			files_path: @@files_path,
+			save_to_file: @@save_to_file,
+			save_to_db: @@save_to_db,
+			incoming_params: @@incoming_params,
+			output_params: @@output_params,
+			processing_time: @@processing_time,
+			current_user: @@current_user,
+			date_time: @@date_time,
+			called_method: @@called_method,
+			class_name: @@class_name
+		}		
+	end
 end
--- oxy_logger.gemspec
+++ oxy_logger.gemspec
@@ -6,11 +6,11 @@ require "oxy_logger/version"
 Gem::Specification.new do |spec|
   spec.name          = "oxy_logger"
   spec.version       = OxyLogger::VERSION
-  spec.authors       = ["Paqwtv"]
+  spec.authors       = ["Vetal, Vlad, Dima"]
   spec.email         = ["paqwtv@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
+  spec.summary       = %q{Gem, wich advise you an ability to write loggs}
+  spec.description   = %q{Logger gem}
   spec.homepage      = "TODO: Put your gem's website or public repo URL here."
   spec.license       = "MIT"
 
@@ -33,4 +33,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.15"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency "yard"
 end
