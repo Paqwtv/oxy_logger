cca95ab142885f4bf8974a5c251f9e2a5340ee7b
Max <madmacj@gmail.com>
2017-09-06 18:23:43 +0300

    Fix typo

--- config/logger_init.rb
+++ config/logger_init.rb
@@ -31,27 +31,27 @@ end
 
 # Default configure Logger GEM
 OxyLogger.configure do |config|
-	# @param value [String] - путь куда сохранять логи
-	# @example
-	#  config.files_path = Rails.root + "/log/logged_data" #the same things in example 
+  # @param value [String] - путь куда сохранять логи
+  # @example
+  #  config.files_path = Rails.root + "/log/logged_data" #the same things in example 
   config.files_path = Rails.root.join('log', 'logged_data')
   # @note - сохранение логов локально в файл или в базу данных
   # @param value [String] - "file" or "db"
   # @example
   #  config.save_to_file_of_db = "db"
   config.save_to = "file"
-	# @note - входящие параметры
-	config.incoming_params = true
-	# @note - исходящие параметры
-	config.output_params = true
-	# @note - время обработки запроса
-	config.processing_time = true
-	# @note - дата и время
-	config.date_time = true
-	# @note - вызываемый метод (action)
-	config.called_method = true
-	# @note - вызываемый класс
-	config.class_name = true
+  # @note - входящие параметры
+  config.incoming_params = true
+  # @note - исходящие параметры
+  config.output_params = true
+  # @note - время обработки запроса
+  config.processing_time = true
+  # @note - дата и время
+  config.date_time = true
+  # @note - вызываемый метод (action)
+  config.called_method = true
+  # @note - вызываемый класс
+  config.class_name = true
 
-	config.rails_app = Rails.application
+  config.rails_app = Rails.application
 end
--- lib/oxy_logger.rb
+++ lib/oxy_logger.rb
@@ -2,128 +2,128 @@ require "oxy_logger/version"
 require "oxy_logger/writer"
 
 module OxyLogger
-	module Sys
-		def logg_it my_names
-			my_names.each do |my_name|
-			method_hook( my_name, :before => :log_befor )
-			method_hook( my_name, :after => :log_after )
-		  end
-		end
-	end
+  module Sys
+    def logg_it my_names
+      my_names.each do |my_name|
+      method_hook( my_name, :before => :log_befor )
+      method_hook( my_name, :after => :log_after )
+      end
+    end
+  end
 
-	module Helper
-	  def log_befor method_name, *args
-	  		first_data = {}
-	  		type = self.class.superclass.to_s
-	  		first_data[:start] = DateTime.now
-			first_data[:name] = method_name
-			first_data[:class_name] = self.class.name
-			puts "1>>>>>>>>"
-			if type == "ApplicationRecord"
-				first_data[:type] = :model
-			else
-				first_data[:type] = :controller
-			end
-			puts "2>>>>>>>>"
-			first_data[:params] = first_data[:type] == :model ?
-				first_data[:args] = args.inspect :
-				first_data[:args] = params
-			
-			puts first_data
-			# # записать лог
-			OxyLogger::Writer.write first_data
-		end
+  module Helper
+    def log_befor method_name, *args
+        first_data = {}
+        type = self.class.superclass.to_s
+        first_data[:start] = DateTime.now
+      first_data[:name] = method_name
+      first_data[:class_name] = self.class.name
+      puts "1>>>>>>>>"
+      if type == "ApplicationRecord"
+        first_data[:type] = :model
+      else
+        first_data[:type] = :controller
+      end
+      puts "2>>>>>>>>"
+      first_data[:params] = first_data[:type] == :model ?
+        first_data[:args] = args.inspect :
+        first_data[:args] = params
+      
+      puts first_data
+      # # записать лог
+      OxyLogger::Writer.write first_data
+    end
 
-		def log_after method_name, *args
-			puts method_name
-			puts args.inspect
-			#     data = DataGetter.получить пост данные
-			#     data = Formatter.format_data first_data
-			# # записать лог
-			#     Writer.write data
+    def log_after method_name, *args
+      puts method_name
+      puts args.inspect
+      #     data = DataGetter.получить пост данные
+      #     data = Formatter.format_data first_data
+      # # записать лог
+      #     Writer.write data
 
-		end
-	end
+    end
+  end
 
-  	@@files_path
-	# @param value [String] - путь куда сохранять логи
-	def self.files_path= value
-		@@files_path = value
-	end
+    @@files_path
+  # @param value [String] - путь куда сохранять логи
+  def self.files_path= value
+    @@files_path = value
+  end
 
-	# @param value [String] - "file" or "db"
-		@@save_to_file_or_db
-	def self.save_to_file_or_db= value
-		@@save_to_file_or_db = value
-	end
+  # @param value [String] - "file" or "db"
+    @@save_to_file_or_db
+  def self.save_to_file_or_db= value
+    @@save_to_file_or_db = value
+  end
 
-		@@incoming_params
-	# @param bool [Bollean] - входящие параметры
-	def self.incoming_params= bool
-		@@incoming_params = bool
-	end
+    @@incoming_params
+  # @param bool [Bollean] - входящие параметры
+  def self.incoming_params= bool
+    @@incoming_params = bool
+  end
 
-		@@output_params
-	# @param bool [Bollean] - исходящие параметры
-	def self.output_params= bool
-		@@output_params = bool
-	end
+    @@output_params
+  # @param bool [Bollean] - исходящие параметры
+  def self.output_params= bool
+    @@output_params = bool
+  end
 
-		@@processing_time
-	# @param bool [Bollean] - время обработки запроса
-	def self.processing_time= bool
-		@@processing_time = bool
-	end
+    @@processing_time
+  # @param bool [Bollean] - время обработки запроса
+  def self.processing_time= bool
+    @@processing_time = bool
+  end
 
-	# @param bool [Bollean] - дата и время
-		@@date_time
-	def self.date_time= bool
-		@@date_time = bool
-	end
+  # @param bool [Bollean] - дата и время
+    @@date_time
+  def self.date_time= bool
+    @@date_time = bool
+  end
 
-	# @param bool [Bollean] - вызываемый метод (экшн)
-	 	@@called_method
-	def self.called_method= bool
-		@@called_method = bool
-	end
+  # @param bool [Bollean] - вызываемый метод (экшн)
+     @@called_method
+  def self.called_method= bool
+    @@called_method = bool
+  end
 
-	# @param bool [Bollean] - вызываемый класс
-	 	@@class_name
- 	def self.class_name= bool
- 		@@class_name = bool
- 	end
-	
-	@@rails_app
- 	def self.rails_app= app
- 		@@rails_app = app
- 	end
+  # @param bool [Bollean] - вызываемый класс
+     @@class_name
+   def self.class_name= bool
+     @@class_name = bool
+   end
+  
+  @@rails_app
+   def self.rails_app= app
+     @@rails_app = app
+   end
 
-	# Конфигурирует сам себя
-	def self.configure
-		yield self		
-	end
+  # Конфигурирует сам себя
+  def self.configure
+    yield self    
+  end
 
-	# @note метод, возвращающий настройки всего приложения после инициализации
-	# @example
-	# 	OxyLogger.config_oxy_hash
-	# @return [Hash] 
-	# 	OxyLogger.config_oxy_hash => {:files_path=>"~/projects/logger", :save_to_file_or_db=>"file",
-	# 	:incoming_params=>true, :output_params=>true, :processing_time=>true,
-	# 	:date_time=>true, :called_method=>true, :class_name=>true}
-	def self.config_oxy_hash
-		{
-			files_path: @@files_path,
-			save_to: @@save_to_file_or_db,
-			incoming_params: @@incoming_params,
-			output_params: @@output_params,
-			processing_time: @@processing_time,
-			date_time: @@date_time,
-			called_method: @@called_method,
-			class_name: @@class_name
-		}
-	end
+  # @note метод, возвращающий настройки всего приложения после инициализации
+  # @example
+  #   OxyLogger.config_oxy_hash
+  # @return [Hash] 
+  #   OxyLogger.config_oxy_hash => {:files_path=>"~/projects/logger", :save_to_file_or_db=>"file",
+  #   :incoming_params=>true, :output_params=>true, :processing_time=>true,
+  #   :date_time=>true, :called_method=>true, :class_name=>true}
+  def self.config_oxy_hash
+    {
+      files_path: @@files_path,
+      save_to: @@save_to_file_or_db,
+      incoming_params: @@incoming_params,
+      output_params: @@output_params,
+      processing_time: @@processing_time,
+      date_time: @@date_time,
+      called_method: @@called_method,
+      class_name: @@class_name
+    }
+  end
 
-	def self.path_to_log
-		@@files_path
-	end
+  def self.path_to_log
+    @@files_path
+  end
 end
